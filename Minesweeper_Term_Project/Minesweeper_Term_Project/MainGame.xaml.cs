6379f0e774e05ef0b1f505aa49920340ee6bd335
Adrian Piwin <piwina@shernet.sheridancollege.ca>
2019-04-16 22:09:10 -0400

    Created second page with main game. Grid is created along with title of the game.

--- Minesweeper_Term_Project/Minesweeper_Term_Project/MainGame.xaml.cs
+++ Minesweeper_Term_Project/Minesweeper_Term_Project/MainGame.xaml.cs
@@ -13,28 +13,18 @@ using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
 
-// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
 
 namespace Minesweeper_Term_Project
 {
     /// <summary>
     /// An empty page that can be used on its own or navigated to within a Frame.
     /// </summary>
-    public sealed partial class MainPage : Page
+    public sealed partial class MainGame : Page
     {
-        public MainPage()
+        public MainGame()
         {
             this.InitializeComponent();
-        }
-
-        /// <summary>
-        /// Button to start game event
-        /// </summary>
-        /// <param name="sender"></param>
-        /// <param name="e"></param>
-        private void OnStart(object sender, RoutedEventArgs e)
-        {
-            this.Frame.Navigate(typeof(MainGame));
         }
     }
 }
