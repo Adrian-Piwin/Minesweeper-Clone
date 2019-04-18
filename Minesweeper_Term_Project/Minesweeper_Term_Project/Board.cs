243961264d03c7baf09a9afd6c2617386b671ab8
Adrian Piwin <piwina@sheridancollege.ca>
2019-04-18 15:48:39 -0800

    Added Board class and TIle class. WIthin Board class: implemented generation of a 2d list to repersent the board using a predefined difficulty and difficulty modifer - created a number of bombs and have them placed randomly within the list.
    Within TIle class:
    Implemented property for the type of tile and set image method to set the attribute of the class's source image according to what the tile contains

---
+++ Minesweeper_Term_Project/Minesweeper_Term_Project/Board.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Minesweeper_Term_Project
+{
+
+    class Board
+    {
+        // 2D array to repersent the board
+        private List<List<Tile>> boardList;
+
+        private static Random s_randomizer;
+
+        // Size of the board numxnum
+        public static int SIZE_OF_BOARD = 10;
+
+        // Diffifculty of game (Percentage of bombs randomly between chosen difficulty + modifier)
+        public static int DIFFICULTY = 10;
+
+        public static int DIFFICULTY_MODIFIER = 10;
+
+        /// <summary>
+        /// Constructor for board class
+        /// </summary>
+        public Board()
+        {
+            boardList = new List<List<Tile>>();
+
+            s_randomizer = new Random();
+
+            generateBoard();
+        }
+
+        /// <summary>
+        /// Generate board for 2d array
+        /// </summary>
+        private void generateBoard()
+        {
+            int boardSize = SIZE_OF_BOARD * SIZE_OF_BOARD;
+
+            int boardDifficulty = s_randomizer.Next(DIFFICULTY, DIFFICULTY_MODIFIER);
+
+            int tempBomb = boardDifficulty / SIZE_OF_BOARD;
+
+            // Add tiles to board list
+            for (int i = 0; i < SIZE_OF_BOARD; i++)
+            {
+                List<Tile> subList = new List<Tile>();
+                for (int ind = 0; ind < SIZE_OF_BOARD; i++)
+                {
+                    if (tempBomb != 0)
+                    {
+                        subList.Add(new Tile(TileType.bomb));
+                        tempBomb -= 1;
+                    }
+                    else
+                    {
+                        subList.Add(new Tile(TileType.empty));
+                    }
+                }
+
+                tempBomb = boardDifficulty / SIZE_OF_BOARD;
+
+                subList = RandomizeTiles(subList);
+                boardList.Add(subList);
+            }
+
+            // Loop through each tile getting surrounding bombs and setting image
+            foreach (List<Tile> sublist in boardList)
+            {
+                foreach (Tile tile in sublist)
+                {
+                    tile.GetSurroundingBombs(boardList);
+                    tile.SetImage();
+                }
+
+            }
+        }
+
+        /// <summary>
+        /// Random tiles in list
+        /// </summary>
+        /// <param name="tileList"></param>
+        public List<Tile> RandomizeTiles(List<Tile> tileList)
+        {
+            List<Tile> shuffledList = new List<Tile>();
+
+            // randomly remove items from first list
+            // and add them to the second list
+            while (tileList.Count > 0)
+            {
+                int index = s_randomizer.Next(tileList.Count);
+                shuffledList.Add(tileList[index]);
+                tileList.RemoveAt(index);
+            }
+
+            return shuffledList;
+        }
+
+        /// <summary>
+        /// Get method for 2D array of the board list
+        /// </summary>
+        public List<List<string>> BoardList { get; }
+    }
+}
---
+++ Minesweeper_Term_Project/Minesweeper_Term_Project/Tile.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml.Media.Imaging;
+
+namespace Minesweeper_Term_Project
+{
+    /// <summary>
+    /// enum for type of tile
+    /// </summary>
+    enum TileType
+    {
+        empty,
+        number,
+        bomb
+    }
+
+    class Tile
+    {
+        // Type of tile
+        private TileType _tileType;
+
+        // Image sourse
+        public BitmapImage _tileSourceImage;
+
+        // Number of bombs around this tile
+        public byte _surroundingBombs;
+
+        /// <summary>
+        /// Constructor for tile
+        /// </summary>
+        /// <param name="tileType"></param>
+        public Tile(TileType tileType)
+        {
+            _tileType = tileType;
+        }
+
+        /// <summary>
+        /// TODO: Method that finds how many bombs are around the tile, then returning that number
+        /// Also changing type depending if any bombs are or are not surrounding it
+        /// </summary>
+        /// <returns></returns>
+        public void GetSurroundingBombs(List<List<Tile>> boardList)
+        {
+            _surroundingBombs = 0;
+        }
+
+        /// <summary>
+        /// Method to set image depending on what type it is
+        /// </summary>
+        public void SetImage()
+        {
+            switch (_tileType)
+            {
+                case TileType.empty:
+                    _tileSourceImage = new BitmapImage();
+                    break;
+
+                case TileType.bomb:
+                    _tileSourceImage = new BitmapImage();
+                    break;
+
+                case TileType.number:
+                    switch (_surroundingBombs)
+                    {
+                        case 1:
+                            _tileSourceImage = new BitmapImage();
+                            break;
+                        case 2:
+                            _tileSourceImage = new BitmapImage();
+                            break;
+                        case 3:
+                            _tileSourceImage = new BitmapImage();
+                            break;
+                        case 4:
+                            _tileSourceImage = new BitmapImage();
+                            break;
+                        case 5:
+                            _tileSourceImage = new BitmapImage();
+                            break;
+                        case 6:
+                            _tileSourceImage = new BitmapImage();
+                            break;
+                        case 7:
+                            _tileSourceImage = new BitmapImage();
+                            break;
+                        case 8:
+                            _tileSourceImage = new BitmapImage();
+                            break;
+                    }
+                    break;
+                    
+            }
+        }
+
+        /// <summary>
+        /// Get method for _tileType
+        /// </summary>
+        public TileType TileType { get { return _tileType; } }
+    }
+}
