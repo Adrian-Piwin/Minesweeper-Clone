38cecdc838fc17e7f09ea8cc3e765d84a1f8b8e1
Wilbur Wang <wangweib@shernet.sheridancollege.ca>
2019-04-20 15:35:10 -0400

    Added the rest of the number of bombs indicator images.

---
+++ Minesweeper_Term_Project/Minesweeper_Term_Project/Number.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Minesweeper_Term_Project
+{
+    class Number : Tile
+    {
+
+        private List<byte> _numberList;
+        public Number(TileType tileType): base(tileType)
+        {
+
+        }
+    }
+}
